6333d222fa0369db54c7a2374ebebef02dcea0c6
Nathan <nathan.rendall@gmail.com>
2021-07-20 23:51:52 +0100

    attractsMostGuests function test

--- models/park.js
+++ models/park.js
@@ -2,14 +2,25 @@ const Park = function(name, ticketPrice){
     this.name = name;
     this.ticketPrice = ticketPrice;
     this.collectionOfDinosaurs = [];
-};
+}
 
 Park.prototype.addDinosaur = function(dinosaur){
-    this.collectionOfDinosaurs.push(dinosaur)
+    this.collectionOfDinosaurs.push(dinosaur);
 };
 
-Park.prototype.removeDinosaur = function(){
-    this.collectionOfDinosaurs.pop()
+Park.prototype.removeDinosaur = function(dinosaur){
+    let dinoIndex = this.collectionOfDinosaurs.indexOf(dinosaur);
+    this.collectionOfDinosaurs.splice(dinoIndex, 1);
 };
+
+Park.prototype.attractsMostGuests = function(){
+    let currentDino = this.collectionOfDinosaurs[0];
+    for (i=0;i<this.collectionOfDinosaurs.length;i++){
+        if (currentDino.guestsAttractedPerDay < this.collectionOfDinosaurs[i].guestsAttractedPerDay){
+            currentDino = this.collectionOfDinosaurs[i]
+        }
+    }
+    return currentDino
+}
 
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -3,13 +3,10 @@ const Park = require('../models/park.js');
 const Dinosaur = require('../models/dinosaur.js');
 
 describe('Park', function() {
-
   let park;
-  let dinosaur;
 
   beforeEach(function () {
     park = new Park("Jurassic Park", 100)
-    dinosaur = Dinosaur('Dino', 'herbivore', 5);
   });
 
   it('should have a name', function (){
@@ -24,25 +21,53 @@ describe('Park', function() {
     assert.deepStrictEqual(park.collectionOfDinosaurs, [])
   });
 
-  it('should be able to add a dinosaur to its collection', function(){
-    park.addDinosaur(dinosaur)
-    assert.deepStrictEqual(park.collectionOfDinosaurs.length, 1)
-  });
 
-  it('should be able to remove a dinosaur from its collection', function(){
-    park.addDinosaur(dinosaur);
-    park.removeDinosaur();
-    assert.deepStrictEqual(park.collectionOfDinosaurs.length, 0);
-  });
+  describe('with dinosaurs', function(){
 
-  it('should be able to find the dinosaur that attracts the most visitors');
+    let dinosaur;
+    let dinosaur2;
+    let dinosaur3;
+    let dinosaur4;
+    let dinosaur5;
 
-  it('should be able to find all dinosaurs of a particular species');
+    beforeEach(function(){
+      dinosaur = new Dinosaur('Dino', 'herbivore', 5);
+      dinosaur2 = new Dinosaur('Stegosaurus', 'herbivore', 10);
+      dinosaur3 = new Dinosaur('Velociraptor', 'carnivore', 50);
+      dinosaur4 = new Dinosaur('Triceratops', 'herbivore', 15);
+      dinosaur5 = new Dinosaur('Velociraptor', 'carnivore', 60);
+    })
 
-  it('should be able to calculate the total number of visitors per day');
+    it('should be able to add a dinosaur to its collection', function () {
+      park.addDinosaur(dinosaur)
+      assert.deepStrictEqual(park.collectionOfDinosaurs.length, 1)
+    });
 
-  it('should be able to calculate the total number of visitors per year');
+    it('should be able to remove a dinosaur from its collection', function () {
+      park.addDinosaur(dinosaur);
+      park.removeDinosaur(dinosaur);
+      assert.deepStrictEqual(park.collectionOfDinosaurs.length, 0);
+    });
 
-  it('should be able to calculate total revenue for one year');
+    it('should be able to find the dinosaur that attracts the most visitors', function(){
+      park.addDinosaur(dinosaur);
+      park.addDinosaur(dinosaur2);
+      park.addDinosaur(dinosaur3);
+      park.addDinosaur(dinosaur4);
+      park.addDinosaur(dinosaur5);
+      dinoMostGuests = park.attractsMostGuests();
+      assert.deepStrictEqual(dinoMostGuests, dinosaur5);
+    });
+
+    it('should be able to find all dinosaurs of a particular species');
+
+    it('should be able to calculate the total number of visitors per day');
+
+    it('should be able to calculate the total number of visitors per year');
+
+    it('should be able to calculate total revenue for one year');
+
+
+  })
 
 });
